5025648ef79450321f588c9e682dd9cdf33509dc
Alexander Mata <mata.alexander@seiza-ti.cl>
2022-11-15 17:38:46 -0300

    ajuste parametros

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,11 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST || 'localhost',
+      host: process.env.DATABASE_HOST || '72.167.124.128',
       port: parseInt(process.env.DATABASE_PORT) || 3306,
-      username: process.env.DATABASE_USER || 'root',
-      password: process.env.DATABASE_PASSWORD || 'root',
-      database: process.env.DATABASE_NAME || 'eduargam',
+      username: process.env.DATABASE_USER || 'eduargamps',
+      password: process.env.DATABASE_PASSWORD || 'TNHheEcDw0QvMqKnF3qx',
+      database: process.env.DATABASE_NAME || 'eduargamps',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
