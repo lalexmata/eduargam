6309d3f00a847874108d0298d40e70dc47c33fe7
Alexander Mata <mata.alexander@seiza-ti.cl>
2022-11-24 11:39:13 -0300

    sync apagada

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,7 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DATABASE_PASSWORD || 'CRWzibcq3WqkZmpLz0nL',
       database: process.env.DATABASE_NAME || 'railway',
       entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: false,
     }),
   ],
 })
